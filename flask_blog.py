12fc64f36210a143040d80ac462b3490bd02ffc3
Montywilliams <3650@holbertonschool.com>
2022-09-29 09:02:47 -0600

    set up basic web app and added some a route to /about: linked / and /home to the same route: app is initiated using env variables and env set to run in dev mode

---
+++ flask_blog.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/home")
+
+def home():
+    return "<h1>Sup Bro</h1>"
+
+@app.route("/about")
+def about():
+    return "<h1>What the talkin bout?</h1>"
