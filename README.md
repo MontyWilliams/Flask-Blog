bd9842395d266e11f2f522555d27c0f1bb944f39
Montywilliams <3650@holbertonschool.com>
2022-09-29 15:01:48 -0600

    added bootstrap styling to layout

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Following tutorial https://www.youtube.com/watch?v=MwZwr5Tvyxo&list=PL-osiE80TeTs4UjLw5MM6OjgkjFeUxCYH
+
+## Using python in Html
+a code block is created with the following syntax: 
+    {% block body %}
+        <\code here>
+    {% endblock %}
--- flask_blog.py
+++ flask_blog.py
@@ -1,13 +1,31 @@
-from flask import Flask
+#!/usr/bin/env python3
+from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Monty',
+        'title': 'Blog this!',
+        'content': 'This is my blog Bro!',
+        'date_posted': '100 in the future Bruh'
+    },
+     {
+        'author': 'Twill',
+        'title': 'Blog that!',
+        'content': 'This is my Bro\'s blog Bro!',
+        'date_posted': '100 in the past hommie'
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 
 def home():
-    return "<h1>Sup Bro</h1>"
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return "<h1>What the talkin bout?</h1>"
+    return render_template('about.html', title='About')
+
+if __name__ == '__main__':
+    app.run(debug=True)
