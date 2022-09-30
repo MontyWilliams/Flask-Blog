f04ab4df14556beab40b5cb779725d3a02d28796
Montywilliams <3650@holbertonschool.com>
2022-09-30 11:23:57 -0600

    added validators to implement field requirments of wtforms: validator used for user name to set a min and max value

---
+++ forms.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired, Length
+""" This module provides our app with forms
+    the syntax will be diff than regular html
+    as it uses flask_wtf's methods
+"""
+
+class RegistrationForm( FlaskForm):
+    """ The fields will all be methods
+    """
+    username = StringField('Username',
+                            validators=[DataRequired(), Length(min=2, max=20) ])
