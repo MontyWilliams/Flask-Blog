d05d2801a16864c810139f55cbe999b7934c1714
Montywilliams <3650@holbertonschool.com>
2022-09-30 11:40:07 -0600

    import PasswordField from wtfilds and using datarequired and length validators

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, PasswordField
 from wtforms.validators import DataRequired, Length, Email
 """ This module provides our app with forms
     the syntax will be diff than regular html
@@ -14,3 +14,6 @@ class RegistrationForm(FlaskForm):
                            validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
+    password = PasswordField('Password',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    
