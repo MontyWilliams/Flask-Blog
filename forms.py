b40eaea63617ab30476ad50e721b7fada45844e3
Montywilliams <3650@holbertonschool.com>
2022-09-30 11:31:04 -0600

    added email field: imported Email validator and used it with the string field to determine if input string id valid email

--- forms.py
+++ forms.py
@@ -1,13 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, Email
 """ This module provides our app with forms
     the syntax will be diff than regular html
     as it uses flask_wtf's methods
 """
 
-class RegistrationForm( FlaskForm):
+
+class RegistrationForm(FlaskForm):
     """ The fields will all be methods
     """
     username = StringField('Username',
-                            validators=[DataRequired(), Length(min=2, max=20) ])
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
