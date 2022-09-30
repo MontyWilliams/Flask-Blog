53e383dad56186d43db745d842faa9f8c2933d32
Montywilliams <3650@holbertonschool.com>
2022-09-30 12:39:41 -0600

    add confirm_password passwordField: import EqualTo validator: set confirm_password equalto passeord using validator

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired, Length, Email
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 """ This module provides our app with forms
     the syntax will be diff than regular html
     as it uses flask_wtf's methods
@@ -16,4 +16,6 @@ class RegistrationForm(FlaskForm):
                         validators=[DataRequired(), Email()])
     password = PasswordField('Password',
                            validators=[DataRequired(), Length(min=2, max=20)])
+    confirm_password = PasswordField('Confirm Password',
+                           validators=[DataRequired(), EqualTo('password')])
     
