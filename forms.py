53c7c44856fe52835aaaef4daf52de0052f99f08
Montywilliams <3650@holbertonschool.com>
2022-09-30 15:33:23 -0600

    syntax correction

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 """ This module provides our app with forms
     the syntax will be diff than regular html
@@ -8,7 +8,9 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
 class RegistrationForm(FlaskForm):
-    """ The fields will all be methods
+    """ This class provides all the functionality needed to register
+        in th Db. all methods are implementations of the wtforma package 
+        which comes with FlaskForm
     """
     username = StringField('Username',
                            validators=[DataRequired(), Length(min=2, max=20)])
@@ -19,3 +21,14 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password',
                            validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Signup Bruh')    
+
+class LoginForm(FlaskForm):
+    """ Similar to the RegistrationForm bu used for login instead
+    """
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    remember = BooleanField('Remember Me')
+    
+    submit = SubmitField('Signup Bruh')
