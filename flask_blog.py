315ddd821ca8ed9eea31b8cc60ffa654482be32d
Montywilliams <3650@holbertonschool.com>
2022-10-02 08:49:18 -0600

    create user class from db.model: include method in class to return string rep of object

--- flask_blog.py
+++ flask_blog.py
@@ -12,6 +12,18 @@ app.config['SECRET_KEY'] = '4a99b4432d537074f1a38919076236d1'
 app.config['SQLALCHEMY_DATDBASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False default='default.jpg')
+    password = db.Column(db.String(20), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
 posts = [
     {
         'author': 'Monty',
