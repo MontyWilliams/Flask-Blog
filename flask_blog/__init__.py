f90b3d5268ddb58ef04eabe5447a11a26a51d638
Montywilliams <3650@holbertonschool.com>
2022-10-03 06:42:15 -0600

    import and initialize Bcrypt

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from forms import RegistrationForm, LoginForm
+from flask_bcrypt import Bcrypt
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '4a99b4432d537074f1a38919076236d1'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+from flask_blog import routes
