c59087de95838a8ba1e37878104f9fbdbbe6974e
Montywilliams <3650@holbertonschool.com>
2022-10-04 08:03:55 -0600

    update login function to query db instead of using hard coded nested value: get user using query filter db methodd: check hash of user passswoed and form data

--- flask_blog/routes.py
+++ flask_blog/routes.py
@@ -57,9 +57,8 @@ def login():
     """
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash('Login successful!')
-            return redirect(url_for('home'))
-        else:
-            flash('Login failed! U tryna hack the page Bru?', 'danger')
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            
+        flash('Login failed! U tryna hack the page Bru?', 'danger')
     return render_template('login.html', title='Login Bruh', form=form)
