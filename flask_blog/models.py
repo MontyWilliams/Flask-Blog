99508bb3fcf2127e946183cc35394e8408f03ac5
Montywilliams <3650@holbertonschool.com>
2022-10-04 07:54:42 -0600

    import UserMixin to satisfy login extension requirments: import UserMixin class into User class aling with the db model

--- flask_blog/models.py
+++ flask_blog/models.py
@@ -1,5 +1,6 @@
 from flask_blog import db, login_manager
 from datetime import datetime
+from flask_login import UserMixin
 """ These are all the models
 """
 
@@ -8,7 +9,7 @@ def load_user (user_id):
     return User.query.get(int(user_id))
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     """ User db model for users in the app
     """
     id = db.Column(db.Integer, primary_key=True)
