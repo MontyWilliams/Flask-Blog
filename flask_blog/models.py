d872fb88fed6540252bf56793f7b5083577f7e44
Montywilliams <3650@holbertonschool.com>
2022-10-04 07:50:41 -0600

    create load user function that gers user by id: add it ass a decorator

--- flask_blog/models.py
+++ flask_blog/models.py
@@ -1,7 +1,11 @@
-from flask_blog import db
+from flask_blog import db, login_manager
 from datetime import datetime
 """ These are all the models
 """
+
+@login_manager.user_loader
+def load_user (user_id):
+    return User.query.get(int(user_id))
 
 
 class User(db.Model):
