685a2afe93fbb6143f2aba12de2ae881f5b43a40
Montywilliams <3650@holbertonschool.com>
2022-10-05 10:01:20 -0600

    create update user form: chedk that the user has enterd a new value

--- flask_blog/forms.py
+++ flask_blog/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flask_blog.models import User
+from flask_login import current_user
 """ This module provides our app with forms
     the syntax will be diff than regular html
     as it uses flask_wtf's methods.
@@ -44,3 +45,25 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Remember Me')
     
     submit = SubmitField('Signup')
+
+class UpdateAccount(FlaskForm):
+    """ This class provides all the functionality needed to update 
+        the user account settings
+    """
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username already in use Bruh!')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is already in use Bruh Bruh') 
