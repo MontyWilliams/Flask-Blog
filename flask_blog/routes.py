6f1edae908d804edb203799141da1c6e33d1f5f7
Montywilliams <3650@holbertonschool.com>
2022-10-05 12:14:21 -0600

    upfate account form to get the username fata and diaplay it in the input text

--- flask_blog/routes.py
+++ flask_blog/routes.py
@@ -90,5 +90,8 @@ def account():
         db.session.commit()
         flash('Account updated successfully' 'success')
         return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', tittle="Account", image_file=image_file, form=form)
