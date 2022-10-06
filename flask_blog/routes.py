4206b56da3cbc28d584d17cadb673b869ac23ad6
Montywilliams <3650@holbertonschool.com>
2022-10-06 17:41:53 -0600

    use Pillow image to automatically resize image on upload

--- flask_blog/routes.py
+++ flask_blog/routes.py
@@ -2,6 +2,7 @@
 from crypt import methods
 import secrets
 import os
+from PIL import Image
 from flask_blog.models import User, Post
 from flask_blog import app, db, bcrypt
 from flask import render_template, url_for, flash, redirect, request
@@ -88,7 +89,12 @@ def save_picture(form_picture):
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
     picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
-    form_picture.save(picture_path)
+    
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+
+    i.save(picture_path)
 
     return picture_fn
 
