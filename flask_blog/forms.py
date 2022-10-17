102f1083345a952b42f9e08957f15caaf9f83e99
Montywilliams <3650@holbertonschool.com>
2022-10-17 01:25:59 -0600

    add form to reset password

--- flask_blog/forms.py
+++ flask_blog/forms.py
@@ -75,3 +75,17 @@ class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     content = TextAreaField('Content', validators=[DataRequired()])
     submit = SubmitField('Post')
+
+
+class requestResetForm(FlaskForm):
+    """ form for changing password
+    """
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError('Email dont exist bru') 
+
