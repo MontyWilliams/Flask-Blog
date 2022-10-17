470ae31da6fd6cb51ab24975dc18598e972b1e3a
Montywilliams <3650@holbertonschool.com>
2022-10-17 02:01:23 -0600

    add function <send_reset_enail> to send user email to verify password change: add

--- flask_blog/routes.py
+++ flask_blog/routes.py
@@ -8,7 +8,8 @@ from flask_blog.models import User, Post
 from flask_blog import app, db, bcrypt
 from flask_cors import CORS
 from flask import render_template, url_for, flash, redirect, request, abort
-from flask_blog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
+from flask_blog.forms import (RegistrationForm, LoginForm, UpdateAccountForm,
+                              PostForm, RequestResetForm, ResetPasswordForm)
 from flask_login import login_user, current_user, logout_user, login_required
 
 """ This is the routing page for the new modular design
@@ -174,3 +175,23 @@ def user_posts(username):
         .order_by(Post.date_posted.desc())\
         .paginate(per_page=5)
     return render_template('user_posts.html', posts=posts, user=user)
+
+
+@app.route("/reset_password", methods=['GET', 'POST'])
+def reset_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RequestResetForm()
+    return render_template('reset_request.html', title='Reset Password', form=form)
+
+
+@app.route("/reset_password/<token>", methods=['GET', 'POST'])
+def reset_token(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    user = User.verify_reset_token(token)
+    if not user:
+        flash('U took to long, token expired Bruh')
+        return redirect(url_for('reset_request'))
+    form = ResetPasswordForm()
+    return render_template('reset_token.html', title='Reset Password', form=form)
