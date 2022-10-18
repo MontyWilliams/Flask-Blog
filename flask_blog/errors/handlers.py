456cefe62c7962ea8dae9d9703acb78962f23fd7
Montywilliams <3650@holbertonschool.com>
2022-10-18 15:27:33 -0600

    add error routes for 403 and 503

--- flask_blog/errors/handlers.py
+++ flask_blog/errors/handlers.py
@@ -1,9 +1,26 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+""" In flask you can return a second value in the route
+    for the status code, the default is 200 so for custom errors
+    we set the error route.
+"""
 
 errors = Blueprint('errors', __name__)
 
 
 @errors.app_errorhandler(404)
 def error_404(error):
-    """ Route to handle 404 error"""
-    return render_template('errors.404.html')
+    """ Route to handle 404 error """
+    return render_template('errors/404.html'), 404
+
+
+@errors.app_errorhandler(403)
+def error_403(error):
+    """ Route to handle 403 error """
+    return render_template('errors/403.html'), 403
+
+
+@errors.app_errorhandler(503)
+def error_503(error):
+    """ Route to handle 503 error """
+    return render_template('errors/503.html'), 503
+    
