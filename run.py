1dc27ace95c1ab344be7a38a58ada3c0021c4eec
Montywilliams <3650@holbertonschool.com>
2022-10-18 14:58:56 -0600

    import create_app and use it to initialize the app with all our configuration

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python 3
-from flask_blog import app
+from flask_blog import create_app
 """ This file is used to run the app
     It imprts based on the contents of __init__
     file which contains the app
 """
+
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
