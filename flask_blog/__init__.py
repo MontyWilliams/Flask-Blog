6b075cac4cf836bff613c9359dca70fd960904e9
Montywilliams <3650@holbertonschool.com>
2022-10-18 13:59:13 -0600

    hard coses values because I couldnt enviornment variables set I guesse this will be a secufrity lesson

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -5,24 +5,16 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
-
-import os
- 
+from flask_blog.config import Config
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '4a99b4432d537074f1a38919076236d1'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config.from_object(Config)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'users.login'
 login_manager.login_message_category ='info'
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USE_SSL'] = False
-app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
-app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+
 mail = Mail(app)
 
 from flask_blog.users.routes import users
